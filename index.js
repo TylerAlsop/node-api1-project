6ad36225e0b9d92e4aea185c8b643e2d000affae
Tyler Alsop <tylerjalsop@gmail.com>
2020-04-08 19:11:43 -0600

    Created and built database.js. Made some small changes to the port.

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const server = express();
 
 server.use(express.json())
 
+const port = 5000
 
-server.listen(5000, () => {
-    console.log("Server started on port 3000")
+
+server.listen(port, () => {
+    console.log(`Server started on port ${port}`)
 });
 
 //////////////// get ////////////////
