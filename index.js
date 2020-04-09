d56d63419208bdf587053eb476ff297fdaf779da
Tyler Alsop <tylerjalsop@gmail.com>
2020-04-08 18:23:15 -0600

    Added most of the functionalities in the MVP. If I didn't know how to do it I added the text for that MVP requiremnt below the specific CRUD operation it was referring to and commented it out. Some of them I made attempts on and commented out the attempt.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ server.use(express.json())
 
 
 server.listen(3000, () => {
-    console.log("Server started on port 8080")
+    console.log("Server started on port 3000")
 });
 
 //////////////// get ////////////////
@@ -20,7 +20,15 @@ server.get("/api/", (req, res) => {
 server.get("/api/users", (req, res) => {
     res.json({ message: "Welcome to the route displaying the users." })
     const users = db.getUsers();
-    res.json(users);
+    
+    if (users) {
+        res.json(users);
+    } else {
+        res.status(500).json({
+            errorMessage: "The users information could not be retrieved."
+        })
+    };
+
 });
 
 server.get("/users/:id", (req, res) => {
@@ -33,10 +41,18 @@ server.get("/users/:id", (req, res) => {
         res.json(user)
     } else {
         res.status(404).json({
-            message: "User not found",
+            message: "The user with the specified ID does not exist."
         })
     }
 });
+/*************** Not sure how to do this: *****************
+ * If there's an error in retrieving the user from the database:
+
+respond with HTTP status code 500.
+return the following JSON object: { errorMessage: "The user information could not be retrieved." }. */
+
+
+
 
 //////////////// post ////////////////
 
@@ -53,6 +69,8 @@ server.post("/api/users", (req, res) => {
     })
 
     res.status(201).json(newUser);
+
+
     /* if (!res) {
         res.status(500).json({
             errorMessage: "There was an error while saving the user to the database."
@@ -61,30 +79,57 @@ server.post("/api/users", (req, res) => {
     */
 })
 
-/*If there's an error while saving the user:
+/*************** See attempt above *****************
+ * If there's an error while saving the user:
 
 respond with HTTP status code 500 (Server Error).
 return the following JSON object: { errorMessage: "There was an error while saving the user to the database" }. */
 
+
+
 //////////////// put & patch ////////////////
 
-server.patch("/api/users/:id", (req, res) => {
+server.put("/api/users/:id", (req, res) => {
     const user = db.getUserById(req.params.id)
 
     if (user) {
         const updatedUser = db.updateUser(user.id, {
             name: req.body.name || user.name
+            bio: req.body.bio || user.bio
         });
 
         res.json(updatedUser)
     } else {
         res.status(404).json({
-            message: "User does not exist. Make sure you have the correct user ID."
+            message: "The user with the specified ID does not exist."
         })
     };
 
-    res.status(201).json(updatedUser)
+    /*
+    if (!req.body.name || !req.body.bio) {
+        res.status(400).json({
+            errorMessage: "Please provide name and bio for the user."
+        })
+    }
+    */
+
+    res.status(200).json(updatedUser)
 })
+
+/*************** See attempt(s) above *****************
+* If the request body is missing the name or bio property:
+
+    respond with HTTP status code 400 (Bad Request).
+    return the following JSON response: { errorMessage: "Please provide name and bio for the user." }.
+
+*If there's an error when updating the user:
+
+    respond with HTTP status code 500.
+    return the following JSON object: { errorMessage: "The user information could not be modified." }.
+*/
+
+
+
 
 //////////////// delete ////////////////
 
@@ -98,7 +143,13 @@ server.delete("/api/users/:id", (req, res) => {
         res.status(204).end()
     } else {
         res.status(404).json({
-            message: "User does not exist. Make sure you have the correct user ID."
+            message: "The user with the specified ID does not exist."
         })
     };
 })
+
+/*************** Not sure how to do this: *****************
+If there's an error in removing the user from the database:
+
+respond with HTTP status code 500.
+return the following JSON object: { errorMessage: "The user could not be removed" }. */
